52055c90a15ecd2d1bf113b95353d7ceab121de0
Ahmed_Qureshi <aa10980@gmail.com>
2023-02-16 10:25:05 +0530

    bat file created

---
+++ paramiko_ssh_to_win_linux/paramiko_push_to_servers.bat
@@ -0,0 +1,8 @@
+echo Activating Python environment...
+call "D:\Python Projects\paramiko_ssh_to_win_linux\Scripts\activate.bat"
+echo Python environment activated.
+echo Running Python script...
+python "D:\Python Projects\paramiko_ssh_to_win_linux\Scripts\paramiko_ssh_to_win_linux\main.py"
+echo Python script complete.
+pause
+
